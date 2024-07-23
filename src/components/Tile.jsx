a30b4d75bb57b20ff413c6102a6549d1da87e4a3
Prajith <prajith26@yahoo.co.in>
2024-07-23 12:59:44 +0530

    code pushed

---
+++ src/components/Tile.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line react/prop-types
+function Tile({ className, value, onClick, playerTurn }) {
+  let hoverClass = null;
+
+  if (value == null && playerTurn != null) {
+    hoverClass = `${playerTurn.toLowerCase()}-hover`;
+  }
+  return (
+    <div onClick={onClick} className={`tile ${className} ${hoverClass}`}>
+      {value}
+    </div>
+  );
+}
+
+export default Tile;
